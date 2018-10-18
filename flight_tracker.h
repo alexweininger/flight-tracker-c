8ddc966c8556661481aff2a3d00fcd50cf921ab8
alexweininger <alex.weininger@live.com>
2018-10-18 14:54:37 -0700

    added name

--- flight_tracker.h
+++ flight_tracker.h
@@ -1,13 +1,15 @@
 /**
  * flight_tracker.h
+ * author: Alex Weininger
+ * modified: 10/18/2018
  */
 
 #include "Node.h"
 
 // function declarations
-int validateCommand(char);
-void readFile(Node *, FILE *);
+void addFlightFromCLI(Node **, char *);
+void deleteFlightFromCLI(Node **, char *);
+int userInput(Node **);
 Node *getFlightsFile(Node **, FILE *);
 int saveToFile(Node *, char *);
 void help();
-int userInput(Node **);
